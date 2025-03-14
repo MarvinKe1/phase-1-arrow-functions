085c2ea925be0a5cde7727428553fb785ea2f1c7
Marvin Mango <marvinke2001@gmail.com>
2025-03-14 13:01:43 +0300

    Arrow Functions

---
+++ index.js
@@ -0,0 +1,16 @@
+// Arrow Functions
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+// Increment and Decrement
+const increment = n => n + 1;
+const decrement = n => n - 1;
+
+// Parsing Functions
+const makeInt = n => parseInt(n, 10);
+const preserveDecimal = n => parseFloat(n);
+
+// Square Function
+const square = x => x * x;
